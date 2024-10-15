eddafe1cf8935fd25d107645168ace3f65e1064c
Patrick Steinhardt <ps@pks.im>
2024-10-15 08:44:14 +0200

    Makefile: use "generate-perl.sh" to massage Perl library
    Extend "generate-perl.sh" such that it knows to also massage the Perl
    library files. There are two major differences:
    
      - We do not read in the Perl header. This is handled by matching on
        whether or not we have a Perl shebang.
    
      - We substitute some more variables, which we read in via our
        GIT-BUILD-OPTIONS.
    
    Adapt both our Makefile and the CMake build instructions to use this.
    
    Signed-off-by: Patrick Steinhardt <ps@pks.im>

--- generate-perl.sh
+++ generate-perl.sh
@@ -17,10 +17,20 @@ OUTPUT="$5"
 . "$GIT_BUILD_OPTIONS"
 
 sed -e '1{' \
+    -e "	/^#!.*perl/!b" \
     -e "	s|#!.*perl|#!$PERL_PATH|" \
     -e "	r $PERL_HEADER" \
     -e '	G' \
     -e '}' \
-    -e "s/@GIT_VERSION@/$GIT_VERSION/g" \
+    -e "s|@GIT_VERSION@|$GIT_VERSION|g" \
+    -e "s|@LOCALEDIR@|$PERL_LOCALEDIR|g" \
+    -e "s|@NO_GETTEXT@|$NO_GETTEXT|g" \
+    -e "s|@NO_PERL_CPAN_FALLBACKS@|$NO_PERL_CPAN_FALLBACKS|g" \
     "$INPUT" >"$OUTPUT"
-chmod a+x "$OUTPUT"
+
+case "$(basename "$INPUT")" in
+*.perl)
+	chmod a+x "$OUTPUT";;
+*)
+	;;
+esac
